9552703bd34e57cb5b178032ee91edde8cdec36e
gabonbon0528 <judy860528@gmail.com>
2023-04-05 21:02:08 +0800

    src js added

--- js/all.js
+++ js/all.js
@@ -2,3 +2,8 @@ window.addEventListener("load", function(){
     let load = document.getElementById("load");
     load.style.display = "none";
 });
+
+let top_btn = document.getElementById('top_botton');
+top_btn.addEventListener('click', function(){
+    window.scrollY = 0;
+});
