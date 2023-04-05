4401471cc15e1d3acfea6b25402cabe15f66e779
gabonbon0528 <judy860528@gmail.com>
2023-04-05 21:21:20 +0800

    load animation html inserted

--- js/all.js
+++ js/all.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function(){
+window.addEventListener('load', function(){
     let load = document.getElementById("load");
     load.style.display = "none";
 });
---
+++ js/product_list.js
@@ -0,0 +1,8 @@
+let all_product_btn = document.getElementById('all_product_btn');
+all_product_btn.addEventListener('click', function(){
+    e.preventDefault();
+    console.log('aaa')
+    // window.scrollY = 600;
+});
+
+console.log(window.scrollY);
