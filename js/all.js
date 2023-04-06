cfb6c8be0b656589bb7516a96039f805a7ac3a0a
gabonbon0528 <judy860528@gmail.com>
2023-04-06 16:26:01 +0800

    rwd hamb added

--- js/all.js
+++ js/all.js
@@ -8,3 +8,18 @@ top_btn.addEventListener('click', function(e){
     e.preventDefault();
     window.scrollTo(0, 0);
 });
+
+$(function(){
+    $('.hamb').click(function(){
+        // console.log('aaa');
+        $('nav > ul').toggleClass('-shown');
+        $('.overlay').toggleClass('shown');
+
+    });
+
+    $('.overlay').click(function(){
+        $('nav > ul').toggleClass('-shown');
+        $('.overlay').toggleClass('shown');
+    })
+    
+});
